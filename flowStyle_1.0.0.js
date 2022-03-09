c8a8601a7cbee026d810b56982bb81bc7b001de6
Konrad52 <kisskonrad25@outlook.hu>
2022-03-09 19:12:58 +0100

    Added fsMsg()

--- flowStyle_1.0.0.js
+++ flowStyle_1.0.0.js
@@ -18,6 +18,9 @@
 
 */
 
+// Global variables.
+var messagePlacer = undefined;
+
 // Setup for the sidebars. A sidebar must be declared within a header, and with that
 // it's height will be calculated accordingly.
 "SETUP";
@@ -33,6 +36,14 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
         bar.style.height = "calc(100vh - " + header.clientHeight + "px)";
     });
+
+    messagePlacer = document.createElement("fs-msg-content");
+    const header = document.getElementsByTagName("fs-header")[0];
+    if (typeof(header) != 'undefined') {
+        header.appendChild(messagePlacer);
+        messagePlacer.style.height = "calc(100vh - " + header.clientHeight + "px)";
+    } else
+        body.appendChild(messagePlacer);
 });
 
 // A function for the button that toggles a sidebar. Requires the ID aka name of the sidebar element.
@@ -43,4 +54,47 @@ function fsToggleSidebar(name) {
         sidebar.classList.remove('fs-opened-sidebar');
     else
         sidebar.classList.add('fs-opened-sidebar');
+}
+
+// Throws an info msg into the msg-content tag.
+// type: string
+// desc: string
+function fsMsg(type, desc) {
+    const message = document.createElement("fs-msg");
+
+    const titleDom = document.createElement("h6");
+    switch (type) {
+        case 'info':
+            titleDom.innerText = 'Info';
+            message.classList.add("fs-msg-info");
+            break;
+        case 'alert':
+            titleDom.innerText = 'Alert';
+            message.classList.add("fs-msg-alert");
+            break;
+        case 'error':
+            titleDom.innerText = 'Error';
+            message.classList.add("fs-msg-error");
+            break;
+    }
+    titleDom.style.textAlign = "left";
+    titleDom.style.marginBottom = "0.25em";
+    message.appendChild(titleDom);
+
+    const descDom = document.createElement("p");
+    descDom.innerText = desc;
+    descDom.style.padding = "0";
+    descDom.style.margin = "0";
+    message.appendChild(descDom);
+
+    messagePlacer.appendChild(message);
+    setTimeout(() => {
+        message.classList.add("fs-msg-shown");
+        setTimeout(() => {
+            message.classList.remove("fs-msg-shown");
+            setTimeout(() => {
+                message.remove();
+            }, 300);
+        }, 4000);
+    }, 10);
 }
